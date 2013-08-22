4aa723ca3e9aede4c9dc8c94e4843caa4102ed7a
Ned KY <nkaidayip@gmail.com>
2013-08-22 15:59:25 -0500

    added PhotoUploader

--- config/uploads.rb
+++ config/uploads.rb
@@ -4,3 +4,5 @@ require 'carrierwave/orm/activerecord'
 CarrierWave.configure do |config|
   config.store_dir = 'public/uploads/'
 end
+
+Dir[APP_ROOT.join('lib', 'uploaders', '*.rb')].each{ |file| require file }
